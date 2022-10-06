c81f6daffa966ab296559058f6cef1cc470f96cf
Mayank Yadav <mayankyadav891999@gmail.com>
2022-10-06 12:25:48 +0530

    using double quotes instead of single

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 export const audioUpdateDuration = 5000
 export const locUpdateDuration = 6000
-export const secret = 'asdfasdfasdfasdf'
+export const secret = "asdfasdfasdfasdf"
 export default { }
